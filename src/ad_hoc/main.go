4f82c0321c6fa378c3a04da7e6fec664ef9a3307
Encephala <jonathan_rietveld@hotmail.com>
2024-07-10 18:35:02 +0200

    Program for ad hoc db manipulation

---
+++ src/ad_hoc/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"terminaccounting/entries"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db := sqlx.MustConnect("sqlite3", "test.db")
+
+	rows := []entries.EntryRow{
+		{
+			Id:         0,
+			Entry:      1,
+			Ledger:     1,
+			Account:    nil,
+			Document:   nil,
+			Value:      entries.DecimalValue{Whole: 6, Fractional: 9},
+			Reconciled: false,
+		},
+		{
+			Id:         0,
+			Entry:      1,
+			Ledger:     2,
+			Account:    nil,
+			Document:   nil,
+			Value:      entries.DecimalValue{Whole: -6, Fractional: 9},
+			Reconciled: false,
+		},
+	}
+
+	fmt.Println("Inserting:", rows)
+	// fmt.Println(entries.InsertRows(db, rows))
+
+	a, err := entries.SelectRows(db)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%#+v\n", a)
+}
