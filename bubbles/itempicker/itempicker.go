2d0e333af7d234f1ce06224fdf911361cd41fac8
Encephala <jonathan_rietveld@hotmail.com>
2024-07-14 23:30:40 +0200

    Create itempicker bubble

---
+++ bubbles/itempicker/itempicker.go
@@ -0,0 +1,69 @@
+package itempicker
+
+import (
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type Item interface {
+	fmt.Stringer
+}
+
+type Model struct {
+	Items      []Item
+	activeItem int
+}
+
+type ItemSelectedMsg struct {
+	Item
+}
+
+func New() *Model {
+	return &Model{
+		Items: make([]Item, 0),
+	}
+}
+
+func (m *Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m *Model) Update(message tea.Msg) (tea.Model, tea.Cmd) {
+	switch message := message.(type) {
+	case tea.KeyMsg:
+		switch message.Type {
+		case tea.KeyCtrlN:
+			m.activeItem++
+
+			if m.activeItem >= len(m.Items) {
+				m.activeItem = 0
+			}
+
+		case tea.KeyCtrlP:
+			m.activeItem--
+
+			if m.activeItem < 0 {
+				m.activeItem = len(m.Items) - 1
+			}
+
+		case tea.KeyEnter:
+			return m, func() tea.Msg {
+				return ItemSelectedMsg{
+					Item: m.Items[m.activeItem],
+				}
+			}
+		}
+	}
+
+	return m, nil
+}
+
+func (m *Model) View() string {
+	if len(m.Items) == 0 {
+		return lipgloss.NewStyle().Italic(true).Render("No items")
+	}
+
+	return m.Items[m.activeItem].String()
+}
---
+++ bubbles/itempicker/main/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bubbles/itempicker"
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type testModel struct {
+	model *itempicker.Model
+}
+
+type Item struct {
+	string
+}
+
+func (i Item) String() string {
+	return i.string
+}
+
+func main() {
+	model := &testModel{
+		model: itempicker.New(),
+	}
+
+	items := []itempicker.Item{
+		Item{"first"},
+		Item{"second"},
+		Item{"third"},
+	}
+	model.model.Items = items
+
+	_, err := tea.NewProgram(model).Run()
+
+	fmt.Printf("Finished: %v\n", err)
+}
+
+func (tm *testModel) Init() tea.Cmd {
+	return tm.model.Init()
+}
+
+func (tm *testModel) Update(message tea.Msg) (tea.Model, tea.Cmd) {
+	switch message := message.(type) {
+	case tea.KeyMsg:
+		if message.Type == tea.KeyCtrlC {
+			return tm, tea.Quit
+		}
+	}
+
+	model, cmd := tm.model.Update(message)
+	tm.model = model.(*itempicker.Model)
+	return tm, cmd
+}
+
+func (tm *testModel) View() string {
+	return tm.model.View()
+}
