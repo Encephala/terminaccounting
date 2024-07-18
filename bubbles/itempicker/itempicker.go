4834a3924919e9c1361899cf64716e0c0edaf0cb
Encephala <jonathan_rietveld@hotmail.com>
2024-07-18 16:34:52 +0200

    Make itempicker constructor take its items

--- bubbles/itempicker/itempicker.go
+++ bubbles/itempicker/itempicker.go
@@ -12,7 +12,7 @@ type Item interface {
 }
 
 type Model struct {
-	Items      []Item
+	items      []Item
 	activeItem int
 }
 
@@ -20,9 +20,9 @@ type ItemSelectedMsg struct {
 	Item
 }
 
-func New() Model {
+func New(items []Item) Model {
 	return Model{
-		Items: make([]Item, 0),
+		items: items,
 	}
 }
 
@@ -37,7 +37,7 @@ func (m Model) Update(message tea.Msg) (Model, tea.Cmd) {
 		case tea.KeyCtrlN:
 			m.activeItem++
 
-			if m.activeItem >= len(m.Items) {
+			if m.activeItem >= len(m.items) {
 				m.activeItem = 0
 			}
 
@@ -45,13 +45,13 @@ func (m Model) Update(message tea.Msg) (Model, tea.Cmd) {
 			m.activeItem--
 
 			if m.activeItem < 0 {
-				m.activeItem = len(m.Items) - 1
+				m.activeItem = len(m.items) - 1
 			}
 
 		case tea.KeyEnter:
 			return m, func() tea.Msg {
 				return ItemSelectedMsg{
-					Item: m.Items[m.activeItem],
+					Item: m.items[m.activeItem],
 				}
 			}
 		}
@@ -61,9 +61,9 @@ func (m Model) Update(message tea.Msg) (Model, tea.Cmd) {
 }
 
 func (m Model) View() string {
-	if len(m.Items) == 0 {
+	if len(m.items) == 0 {
 		return lipgloss.NewStyle().Italic(true).Render("No items")
 	}
 
-	return m.Items[m.activeItem].String()
+	return m.items[m.activeItem].String()
 }
