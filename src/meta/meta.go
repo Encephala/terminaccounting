71413952bca303920315bdedb36037760dc1b681
Encephala <jonathan_rietveld@hotmail.com>
2025-11-08 10:55:46 +0100

    Remove outdated comment

--- src/meta/meta.go
+++ src/meta/meta.go
@@ -18,10 +18,5 @@ type App interface {
 	AcceptedModels() map[ModelType]struct{}
 
 	MakeLoadListCmd() tea.Cmd
-	// TODO: I'd like for this to take an int argument, but hard rn because it's only called in
-	// generic DetailView's Init, which doesn't know of any id's.
-	// Think that's circumventable though? DetailView Init can send a messagecmd to make the app init the view
-	// Kinda jank I guess but it works? Asserting view type rn is even more jank imo
-	// Actually much simpler: detailview holds the app and the id already. Easy fix then
 	MakeLoadRowsCmd(modelId int) tea.Cmd
 }
