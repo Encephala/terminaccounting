36cb2350538081f1c56bad4556ddfefc691e9c84
Encephala <jonathan_rietveld@hotmail.com>
2025-12-31 01:14:18 +0100

    Override previous motion if inserting at same path
    Idk why I had the previous logic

--- src/meta/trie.go
+++ src/meta/trie.go
@@ -100,12 +100,8 @@ func (t *Trie[T]) Insert(path []string, value T) (changed bool) {
 			// Actually, if this happens, we can drop all t's children,
 			// because as soon as a motion resolves, it executes
 			if isFinalValue {
-				// NOTE: When t.isLeaf, this does not check if the t.value is different from the provided value,
-				// i.e. it is not possible to update a value in the trie
-				if !t.isLeaf {
-					t.isLeaf = true
-					t.value = value
-				}
+				t.isLeaf = true
+				t.value = value
 			}
 
 			continue
