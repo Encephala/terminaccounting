db93cb2ddbacac952a1edd5e476a2a18653bb02a
Encephala <jonathan_rietveld@hotmail.com>
2026-01-02 19:34:47 +0100

    Improve app switch test

--- src/model_test.go
+++ src/model_test.go
@@ -17,6 +17,14 @@ func initWrapper(t *testing.T) tatesting.TestWrapper {
 	return tat.InitIntegrationTest(t, newTerminaccounting(DB))
 }
 
+func adaptedWait(wrapper tat.TestWrapper, condition func(*terminaccounting) bool) *terminaccounting {
+	genericCondition := func(m tea.Model) bool {
+		return condition(m.(*terminaccounting))
+	}
+
+	return wrapper.Wait(genericCondition).(*terminaccounting)
+}
+
 func adaptedWaitQuit(wrapper tat.TestWrapper, condition func(*terminaccounting) bool) *terminaccounting {
 	genericCondition := func(m tea.Model) bool {
 		return condition(m.(*terminaccounting))
@@ -43,10 +51,31 @@ func TestSwitchAppUnit(t *testing.T) {
 func TestSwitchAppIntegration(t *testing.T) {
 	wrapper := initWrapper(t)
 
+	// Next tab
 	wrapper.Send(tat.KeyMsg("g"), tat.KeyMsg("t"))
-	model := adaptedWaitQuit(wrapper, func(ta *terminaccounting) bool {
+
+	model := adaptedWait(wrapper, func(ta *terminaccounting) bool {
 		return ta.appManager.activeApp == 1
 	})
 
-	assert.Equal(t, model.appManager.activeApp, 1)
+	assert.Equal(t, 1, model.appManager.activeApp)
+
+	// Wrap tabs backwards
+	wrapper.Send(tat.KeyMsg("g"), tat.KeyMsg("T"))
+	wrapper.Send(tat.KeyMsg("g"), tat.KeyMsg("T"))
+
+	model = adaptedWait(wrapper, func(ta *terminaccounting) bool {
+		return ta.appManager.activeApp == 3
+	})
+
+	assert.Equal(t, 3, model.appManager.activeApp)
+
+	// Wrap tabs forwards
+	wrapper.Send(tat.KeyMsg("g"), tat.KeyMsg("t"))
+
+	model = adaptedWait(wrapper, func(ta *terminaccounting) bool {
+		return ta.appManager.activeApp == 0
+	})
+
+	assert.Equal(t, 0, model.appManager.activeApp)
 }
