3916071a35c8399700fce64484abb3a1a8fdc063
Encephala <jonathan_rietveld@hotmail.com>
2026-01-02 18:47:06 +0100

    Implement custom testing framework

--- src/meta/serialisation_test.go
+++ src/meta/serialisation_test.go
@@ -1,16 +1,15 @@
 package meta
 
 import (
+	tat "terminaccounting/tatesting"
 	"testing"
 
-	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestMarshalUnmarshalNotes(t *testing.T) {
-	db := setupDB(t)
+	db := tat.SetupTestDB(t)
 
 	type Test struct {
 		SomeId int   `db:"id"`
@@ -42,15 +41,4 @@ func TestMarshalUnmarshalNotes(t *testing.T) {
 	assert.Equal(t, count, 1)
 
 	assert.Equal(t, result.Notes, expected.Notes)
-}
-
-func setupDB(t *testing.T) *sqlx.DB {
-	t.Helper()
-
-	db := sqlx.MustConnect("sqlite3", ":memory:")
-	_, err := db.Exec(`CREATE TABLE test(id INTEGER NOT NULL, notes TEXT NOT NULL) STRICT;`)
-
-	require.NoError(t, err)
-
-	return db
 }
---
+++ src/tatesting/tatesting.go
@@ -0,0 +1,117 @@
+package tatesting
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/require"
+)
+
+func SetupTestDB(t *testing.T) *sqlx.DB {
+	t.Helper()
+
+	db := sqlx.MustConnect("sqlite3", ":memory:")
+	_, err := db.Exec(`CREATE TABLE test(id INTEGER NOT NULL, notes TEXT NOT NULL) STRICT;`)
+
+	require.NoError(t, err)
+
+	return db
+}
+
+type TestWrapper struct {
+	t *testing.T
+
+	program    *tea.Program
+	asyncModel *asyncModel
+
+	doneChannel chan tea.Model
+}
+
+type asyncModel struct {
+	model tea.Model
+
+	mutex sync.Mutex
+}
+
+func (am *asyncModel) Init() tea.Cmd {
+	am.mutex.Lock()
+	defer am.mutex.Unlock()
+	return am.model.Init()
+}
+
+func (am *asyncModel) Update(message tea.Msg) (tea.Model, tea.Cmd) {
+	am.mutex.Lock()
+	defer am.mutex.Unlock()
+	var cmd tea.Cmd
+	am.model, cmd = am.model.Update(message)
+
+	return am, cmd
+}
+
+func (am *asyncModel) View() string {
+	am.mutex.Lock()
+	defer am.mutex.Unlock()
+	return am.model.View()
+}
+
+func InitIntegrationTest(t *testing.T, model tea.Model) TestWrapper {
+	asyncModel := &asyncModel{model: model}
+
+	program := tea.NewProgram(asyncModel, tea.WithoutRenderer())
+
+	doneChannel := make(chan tea.Model)
+	go func() {
+		final, _ := program.Run()
+		doneChannel <- final
+	}()
+
+	return TestWrapper{
+		t: t,
+
+		program:    program,
+		asyncModel: asyncModel,
+
+		doneChannel: doneChannel,
+	}
+}
+
+func (tw *TestWrapper) Send(messages ...tea.Msg) {
+	for _, message := range messages {
+		tw.program.Send(message)
+	}
+}
+
+// Waits for the provided condition to be met, then quits the program, returning the final program state
+func (tw *TestWrapper) WaitQuit(condition func(tea.Model) bool) tea.Model {
+	ticker := time.NewTicker(time.Millisecond * 50)
+	timeout := time.After(time.Second * 2)
+
+	for {
+		select {
+		case <-timeout:
+			tw.t.Fatalf("test timed out")
+			return nil
+
+		case <-ticker.C:
+			if !(condition(tw.asyncModel.model)) {
+				continue
+			}
+
+			tw.program.Quit()
+
+			finalModel := <-tw.doneChannel
+
+			return finalModel.(*asyncModel).model
+		}
+	}
+}
+
+func KeyMsg(input string) tea.KeyMsg {
+	return tea.KeyMsg{
+		Type:  tea.KeyRunes,
+		Runes: []rune(input),
+	}
+}
