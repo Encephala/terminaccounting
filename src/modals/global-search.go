184944e6e58de2a2c947d17ceb1a0a79bb30de94
Encephala <jonathan_rietveld@hotmail.com>
2026-05-12 01:06:23 +0200

    (actually) Implement filtering in global search

--- src/modals/global-search.go
+++ src/modals/global-search.go
@@ -11,13 +11,92 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/jmoiron/sqlx"
+	"github.com/sahilm/fuzzy"
 )
 
 type searchItem struct {
 	model any
 }
 
-func (item *searchItem) Render(isActive bool) string {
+func (item *searchItem) filterValue() string {
+	var result strings.Builder
+
+	availableLedgers := database.AvailableLedgers()
+	availableAccounts := database.AvailableAccounts()
+	availableJournals := database.AvailableJournals()
+
+	switch model := item.model.(type) {
+	case database.Ledger:
+		result.WriteString(fmt.Sprintf("%d", model.Id))
+		result.WriteString(model.Name)
+		result.WriteString(fmt.Sprintf("%s", model.Type))
+		result.WriteString(model.Notes.Collapse())
+
+		if model.IsAccounts {
+			result.WriteString("isaccounts")
+		}
+
+	case database.Account:
+		result.WriteString(fmt.Sprintf("%d", model.Id))
+		result.WriteString(model.Name)
+		result.WriteString(fmt.Sprintf("%s", model.Type))
+		result.WriteString(model.BankNumbers.Collapse())
+		result.WriteString(model.Notes.Collapse())
+
+	case database.Journal:
+		result.WriteString(fmt.Sprintf("%d", model.Id))
+		result.WriteString(model.Name)
+		result.WriteString(fmt.Sprintf("%s", model.Type))
+		result.WriteString(model.Notes.Collapse())
+
+	case database.Entry:
+		result.WriteString(fmt.Sprintf("%d", model.Id))
+
+		journal := availableJournals[slices.IndexFunc(availableJournals, func(other database.Journal) bool {
+			return other.Id == model.Journal
+		})]
+		result.WriteString(journal.Name)
+
+		result.WriteString(model.Notes.Collapse())
+
+	case database.EntryRow:
+		result.WriteString(fmt.Sprintf("%d", model.Id))
+		result.WriteString(fmt.Sprintf("%d", model.Entry))
+		result.WriteString(model.Date.String())
+
+		ledger := availableLedgers[slices.IndexFunc(availableLedgers, func(other database.Ledger) bool {
+			return other.Id == model.Ledger
+		})]
+		result.WriteString(fmt.Sprintf("%s", ledger.Name))
+
+		if model.Account == nil {
+			result.WriteString("none")
+		} else {
+			account := availableAccounts[slices.IndexFunc(availableAccounts, func(other database.Account) bool {
+				return other.Id == *model.Account
+			})]
+			result.WriteString(fmt.Sprintf("%s", account.Name))
+		}
+
+		result.WriteString(model.Description)
+		if model.Document != nil {
+			result.WriteString(*model.Document)
+		}
+
+		result.WriteString(model.Value.String())
+
+		if model.Reconciled {
+			result.WriteString("reconciled")
+		}
+
+	default:
+		panic(fmt.Sprintf("unexpected searchItem type: %#v", model))
+	}
+
+	return result.String()
+}
+
+func (item *searchItem) render(isActive bool) string {
 	var color lipgloss.Color
 	var text string
 
@@ -41,10 +120,10 @@ func (item *searchItem) Render(isActive bool) string {
 	case database.EntryRow:
 		color = meta.ENTRIESCOLOUR
 		// TODO
-		text = "entryRow"
+		text = "Entryrow"
 
 	default:
-		panic(fmt.Sprintf("unexpected type: %#v", model))
+		panic(fmt.Sprintf("unexpected searchItem type: %#v", model))
 	}
 
 	if isActive {
@@ -62,7 +141,8 @@ type globalSearchModal struct {
 	viewport  viewport.Model
 	activeRow int
 
-	items []searchItem
+	items      []searchItem
+	shownItems []searchItem
 }
 
 func newGlobalSearchModal(DB *sqlx.DB) *globalSearchModal {
@@ -87,7 +167,7 @@ func (sm *globalSearchModal) Update(message tea.Msg) (Modal, tea.Cmd) {
 
 	case meta.NavigateMsg:
 		if message.Direction == meta.DOWN {
-			sm.activeRow = min(sm.activeRow+1, len(sm.items)-1)
+			sm.activeRow = min(sm.activeRow+1, len(sm.shownItems)-1)
 		} else {
 			sm.activeRow = max(sm.activeRow-1, 0)
 		}
@@ -98,8 +178,11 @@ func (sm *globalSearchModal) Update(message tea.Msg) (Modal, tea.Cmd) {
 		var cmd tea.Cmd
 		sm.searchQuery = message.Query
 
-		// TODO: filter items
-		// Also make sure activeRow stays in sane state
+		if message.Query != "" {
+			sm.activeRow = 0
+		}
+
+		sm.updateShownItems()
 
 		return sm, cmd
 
@@ -109,6 +192,8 @@ func (sm *globalSearchModal) Update(message tea.Msg) (Modal, tea.Cmd) {
 		for i, item := range message.Data {
 			sm.items[i] = item.(searchItem)
 		}
+
+		sm.updateShownItems()
 
 		return sm, nil
 
@@ -130,14 +215,32 @@ func (sm *globalSearchModal) View() string {
 	return result.String()
 }
 
-func (sm *globalSearchModal) updateViewportContent() {
-	var lines []string
-
-	for i, item := range sm.items {
-		lines = append(lines, item.Render(sm.activeRow == i))
+func (sm *globalSearchModal) updateShownItems() {
+	if sm.searchQuery == "" {
+		sm.shownItems = sm.items
+		return
 	}
 
-	sm.viewport.SetContent(strings.Join(lines, "\n"))
+	filterValues := make([]string, len(sm.items))
+	for i, item := range sm.items {
+		filterValues[i] = item.filterValue()
+	}
+
+	matches := fuzzy.Find(sm.searchQuery, filterValues)
+
+	sm.shownItems = make([]searchItem, len(matches))
+	for i, match := range matches {
+		sm.shownItems[i] = sm.items[match.Index]
+	}
+}
+
+func (sm *globalSearchModal) updateViewportContent() {
+	var result []string
+	for i, item := range sm.shownItems {
+		result = append(result, item.render(sm.activeRow == i))
+	}
+
+	sm.viewport.SetContent(strings.Join(result, "\n"))
 }
 
 func (sm *globalSearchModal) AllowsInsertMode() bool {
@@ -160,7 +263,9 @@ func (sm *globalSearchModal) MotionSet() meta.Trie[tea.Msg] {
 		var appType *meta.AppType
 		var data any
 
-		activeItem := sm.items[sm.activeRow]
+		// TODO: handle panic if zero items shown
+
+		activeItem := sm.shownItems[sm.activeRow]
 		switch model := activeItem.model.(type) {
 		case database.Ledger:
 			tmp := meta.LEDGERSAPP
